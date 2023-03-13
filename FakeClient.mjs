8ceac7f0b56fcc29252969e193990dcee2c807c0
Erik <erik.raboczki98@gmail.com>
2023-03-13 12:03:34 +0100

    Demonstration is complete.

--- FakeClient.mjs
+++ FakeClient.mjs
@@ -1,31 +1,49 @@
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
+
+// fetch("http://localhost:3001/2").then((response) => {
+//   response.json().then((data) => {console.log(data);})
+// }).catch((error) => {
+//   console.log("Error");
+// })
 
 
-fetch("http://localhost:3001/4").then((response) => {
-  response.json().then((data) => {console.log(data);})
+const fetchSimulation = (number) => {
+  const data = {
+        name: "Professor",
+        skills: ["IT","Math","Communication"],
+        age: 35
+  }
+  const error = "This is an error message!"
+
+  return new Promise((resolve, reject) => {
+    if (number % 2 == 0) {
+        resolve({data:data, number: number})
+    }
+    else {
+        reject(`\t${error} ${number}`)
+    }
+  });
+}
+
+
+async function allTheFunctionsAreHere() {
+  let a =  fetchSimulation(2)
+  let b =  fetchSimulation(4)
+  let c =  fetchSimulation(6)
+  let data = await Promise.all([a,b,c])
+
+  let result = data[0].number + data[1].number + data[2].number
+  console.log("The result is: " + result);
+
+}
+allTheFunctionsAreHere()
+
+fetchSimulation(2).then((data1) => {
+  fetchSimulation(4).then((data2) => {
+    fetchSimulation(6).then((data3) => {
+      let result = data1.number + data2.number + data3.number
+      console.log("The result is: " + result);
+    })
+  })
 })
 
-
-
-
-
-
-
-
-// const fakeFetchData = (number) => {
-//   const data = {
-//         name: "Professor",
-//         skills: ["IT","Math","Communication"],
-//         age: 35
-//   }
-//   const error = "This is an error message!"
-
-//   return new Promise((resolve, reject) => {
-//     if (number % 2 == 0) {
-//         resolve({data:data, number: number})
-//     }
-//     else {
-//         reject(`\t${error} ${number}`)
-//     }
-//   });
-// }
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # CC_JavaScript_Fetch_and_Async
-This is a simple test project. Please navigate to the ...
+This is a simple test project. Please navigate to the this folder and open the terminal, please give the following commands:
+
+-npm i
+-npm start 
+
+
+
+//The npm i will install some of the npm packages that are the dependencies of this project
+//And the npm start will start the Express.js server
+
+You can then open a new terminal and run the FakeClient.mjs file with the:
+
+node .\FakeClient.mjs
+
+command.
+
+
