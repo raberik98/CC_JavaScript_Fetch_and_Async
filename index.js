f11a37f05cb960cabc1cc3f9dc1eb8c25ed74d8b
Erik <erik.raboczki98@gmail.com>
2024-02-05 11:44:23 +0100

    2024.02.05WS

--- index.js
+++ index.js
@@ -14,14 +14,12 @@ app.use("/", (req,res,next) => {
     next()
 })
 
-app.get("/:number", (req,res) => {
-    const number = req.params.number
-    if (number % 2 == 0) {
-        res.json(data)
-    }
-    else {
-        res.status(500).json(error)
-    }
+app.get("/data", (req,res) => {
+    res.json(data)
+})
+
+app.get("/simulateError", (req,res) => {
+    res.status(500).json(error)
 })
 
 app.listen(3000, ()=> {console.log("Backend running at port: 3000");})
